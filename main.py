faf4e137368f4b0fc5b3964aea302e2f3a012140
Euller Macena <euller_macena@id.uff.br>
2019-09-23 21:44:25 -0300

    "Limpei" o codigo aksdop

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
 import pygame
-import plano
 import poligonos
-import quadrosChave
 import transform
 
 
 altura = 580
 largura = 960
-
-v0 = [0, 0, 0]
-v1 = [100, 0, 0]
-v2 = [0, 100, 0]
-a0 = [0, 1]
-a1 = [0, 2]
-a2 = [1, 2]
-
-
-def desenha(figura, janela):
-    arestas = poligonos.get_arestas(figura)
-    for i in range(len(arestas)):
-        pygame.draw.line(janela, (255, 255, 255),
-                           (figura.vertices.vertice[arestas[i][0]][0], figura.vertices.vertice[arestas[i][0]][1]),
-                           (figura.vertices.vertice[arestas[i][1]][0], figura.vertices.vertice[arestas[i][1]][1]),1)
-
 
 clock = pygame.time.Clock()
 
@@ -41,11 +23,6 @@ def main():
     background = background.convert_alpha()
     background.fill((0, 0, 0))
 
-    fig1 = poligonos.get_cubo()
-    fig1 = poligonos.setCentro(fig1)
-    #fig1 = transform.shearing(fig1, 1, 1/2, 1)
-    fig1.setMoveX(True)
-
     fig2 = poligonos.get_zig()
     fig2 = poligonos.setCentro(fig2)
     scale = 10
@@ -60,20 +37,21 @@ def main():
 
         scale = fig2.getScale()
         fig2 = transform.aumentaEscala(fig2, scale, janela, largura, altura)
-
         scale = fig2.getScale()
         fig2 = transform.diminuiEscala(fig2, scale, janela, largura, altura)
-
         fig2 = transform.transladaRotacionando(fig2, janela, largura, altura)
 
         fig2 = transform.aumentaCisalhamento(fig2, janela, 1)
         fig2 = transform.diminuiCisalhamento(fig2, janela, 1)
+        fig2 = transform.transladaRotacionando(fig2, janela, largura, altura)
 
         fig2 = transform.aumentaCisalhamento(fig2, janela, 2)
         fig2 = transform.diminuiCisalhamento(fig2, janela, 2)
+        fig2 = transform.transladaRotacionando(fig2, janela, largura, altura)
 
         fig2 = transform.aumentaCisalhamento(fig2, janela, 3)
         fig2 = transform.diminuiCisalhamento(fig2, janela, 3)
+        fig2 = transform.transladaRotacionando(fig2, janela, largura, altura)
 
 
 main()
